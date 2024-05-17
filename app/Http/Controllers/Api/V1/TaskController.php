d47187d3d0bdcbb16e5a68ecb71584f86160f481
Fazle Ryan Chowdhury <ryan7998@gmail.com>
2024-05-17 12:43:19 -0400

    install api and set routes for tasks

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Task;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 
@@ -13,7 +14,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        return Task::all();
     }
 
     /**
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\V1\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// (use v1 as prefix)
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/tasks', TaskController::class);
+});
+// use apiResource instead of direct get method: 
+// we could use Route::apiResource('/v1/tasks', TaskController::class) if prefix was not used
+// Route::apiResource('/tasks', TaskController::class);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
