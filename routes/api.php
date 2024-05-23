101d958aef696a85cb7ea5ae96a53b701224ccaf
Fazle Ryan Chowdhury <ryan7998@gmail.com>
2024-05-22 22:42:59 -0400

    Using Sanctum for API Token Authentication

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,25 @@
 <?php
 
-use App\Http\Controllers\Api\V1\CompleteTaskController;
-use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\V1\CompleteTaskController;
 
 // (use v1 as prefix)
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('/tasks', TaskController::class);
     Route::patch('/tasks/{task}/complete', CompleteTaskController::class);
 });
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', LoginController::class);
+    Route::post('/logout', LogoutController::class)->middleware('auth:sanctum');
+    Route::post('/register', RegisterController::class);
+});
+
 // use apiResource instead of direct get method: 
 // we could use Route::apiResource('/v1/tasks', TaskController::class) if prefix was not used
 // Route::apiResource('/tasks', TaskController::class);
